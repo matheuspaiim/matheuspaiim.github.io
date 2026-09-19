9c396d322be3d297f7fc8b56875f2139c05bb74f
Matheus <65717016+matheuspaiim@users.noreply.github.com>
2026-09-18 21:35:00 -0300

    Refresh Libras Studio mobile cache after auth fix

--- libras-studio/sw.js
+++ libras-studio/sw.js
@@ -1,4 +1,4 @@
-const CACHE="libras-studio-mobile-v2";
+const CACHE="libras-studio-mobile-v3";
 const SHELL=["./","./index.html","./app.css","./app.js","./config.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
