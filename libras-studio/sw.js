19cae741d2f9426fa5a9bc4fa4f293493662d0c8
Matheus <65717016+matheuspaiim@users.noreply.github.com>
2026-09-18 21:36:17 -0300

    Activate Libras Studio mobile auth fix immediately

--- libras-studio/sw.js
+++ libras-studio/sw.js
@@ -1,5 +1,5 @@
-const CACHE="libras-studio-mobile-v3";
+const CACHE="libras-studio-mobile-v4";
 const SHELL=["./","./index.html","./app.css","./app.js","./config.js","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
+self.addEventListener("install",e=>e.waitUntil(Promise.all([self.skipWaiting(),caches.open(CACHE).then(c=>c.addAll(SHELL))])));
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
