2f6717055c56f8aa86cb44551a7e800e16a6d3a6
Matheus <65717016+matheuspaiim@users.noreply.github.com>
2026-09-19 12:37:33 -0300

    release(android): marcar APK build 9

--- native-android/app/src/main/java/com/matheuspaiim/librasstudio/MainActivity.java
+++ native-android/app/src/main/java/com/matheuspaiim/librasstudio/MainActivity.java
@@ -17,7 +17,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-    private static final String STUDIO_URL = "file:///android_asset/web/index.html?native=android&apk=8";
+    private static final String STUDIO_URL = "file:///android_asset/web/index.html?native=android&apk=9";
     private static final String STUDIO_HOST = "matheuspaiim.github.io";
     private static final String STUDIO_PATH = "/libras-studio/";
     private static final int FILE_CHOOSER_REQUEST = 4301;
